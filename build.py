cdf45aae01fb2edcad7f4744efb84e73126655b1
Graham Hughes <graehu@gmail.com>
2021-11-07 23:38:44 +0000

    openh264
    defaulting to openh264 now, so people can use this commerically.
    the confply / build script has gotten gross. i'll attempt to address
    that at some point soon.
    
    -- ammend --
    
    pushing v2.1.1 specifically
    pushing confply fix

--- build.py
+++ build.py
@@ -24,12 +24,14 @@ config.include_paths = ["godot-cpp/godot-headers",
                         "godot-cpp/include/gen",
 ]
 ff_dir = "FFmpeg/"
-# #todo: add an option to link to build without x264
-# ff_libs = ["avformat", "avcodec", "swresample", "swscale", "avutil"]
-ff_libs = ["avformat", "avcodec", "swresample", "swscale", "avutil", "x264"]
+if "--x264" in config.confply.args:
+    ff_libs = ["avformat", "avcodec", "swresample", "swscale", "avutil", "x264"]
+else:
+    ff_libs = ["avformat", "avcodec", "swresample", "swscale", "avutil", "openh264"]
 ff_lib_dirs = [ff_dir+"lib"+lib for lib in ff_libs]
 config.library_paths.append("recorder/bin/")
 config.library_paths.append("godot-cpp/bin/")
+config.library_paths.append("openh264/")
 config.link_libraries.extend(ff_libs)
 config.link_libraries.append("godot-cpp.linux.debug.64")
 config.compile_commands = True
@@ -41,30 +43,55 @@ config.run_paths = ["'$ORIGIN'"]
 
 def post_load():
     import os
+    import shutil
+    import pathlib
+    libs = pathlib.Path("recorder/bin/").glob("*.so*")
+    for lib in libs:
+        os.remove(lib)
     if not os.path.exists("godot-cpp/bin/libgodot-cpp.linux.debug.64.a"):
         os.system("cd godot-cpp; scons platform=linux generate_bindings=yes -j4")
+        log.normal("godot-cpp bindings generated")
+
+    if "--x264" not in config.confply.args:
+        if not os.path.exists("openh264/lib/libopenh264.so.2.1.1"):
+            os.system("cd openh264; make install-shared OS=linux ARCH=x86_64 PREFIX=$(pwd)")
+            log.normal("openh264 built")
+
     if not os.path.exists(ff_dir+"config.h"):
-        os.system("cd "+ff_dir+"; ./configure --enable-shared --enable-libx264 --enable-gpl")
-    os.system("cd "+ff_dir+"; make")
+        configure = ["--enable-shared", "--disable-programs"]
+        if "--x264" in config.confply.args:
+            os.system("cd "+ff_dir+"; ./configure --enable-shared --enable-libx264 --enable-gpl")
+        else:
+            configure.extend(
+                [
+                    "--enable-libopenh264",
+                ]
+            )
+            os.system("export PKG_CONFIG_PATH=$(pwd)/openh264/lib/pkgconfig; cd "+ff_dir+";./configure "+" ".join(configure))
+    os.system("cd "+ff_dir+";  make")
     log.normal("ffmpeg built")
-    pass
-
-
-def post_run():
-    import shutil
-    import subprocess
-    import os
-    to_path = "demo/recorder"
-    # #todo: maybe don't always do this.
-    if os.path.exists(to_path):
-        shutil.rmtree(to_path)
     for lib_dir in ff_lib_dirs:
         if os.path.exists(lib_dir):
             for obj in os.listdir(lib_dir):
                 if ".so." in obj:
                     cp_file = os.path.join(lib_dir, obj)
                     shutil.copy(cp_file, "recorder/bin/")
-    subprocess.check_output("cp $(find /lib/ -name libx264.so.160) recorder/bin", shell=True)
+    if "--x264" in config.confply.args:
+        os.system("cp $(find /lib/ -name libx264.so.160) recorder/bin")
+    else:
+        shutil.copy("openh264/lib/libopenh264.so", "recorder/bin")
+        shutil.copy("openh264/lib/libopenh264.so.2.1.1", "recorder/bin")
+        shutil.copy("openh264/lib/libopenh264.so.6", "recorder/bin")
+    pass
+
+
+def post_run():
+    import shutil
+    import os
+    to_path = "demo/recorder"
+    # #todo: maybe don't always do this.
+    if os.path.exists(to_path):
+        shutil.rmtree(to_path)
     shutil.copytree("recorder", to_path)
     # test it!
     if "--test" in config.confply.args:
